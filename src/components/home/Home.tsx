c2c1b87ad4f2257985209d37e1354965bdcfbe6b
Edwin Bryan <bbryan211@gmail.com>
2022-01-26 22:02:30 -0600

    Add mint page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -6,11 +6,12 @@ import Vision from '../vision/Vision';
 import Organizations from '../organizations/Organizations';
 
 import logo from '../../assets/img/logo-bg.png';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <>
-        <Header />
+            <Header />
             <div className="section mb-5">
                 <div className="px-4 pt-1 pb-4 mt-1 mb-4 text-center">
                     <img className="d-block mx-auto mb-4" src={logo} alt="Bunks NFT" width="432" height="312" />
@@ -18,7 +19,7 @@ function Home() {
                     <div className="col-lg-6 mx-auto">
                         <p className="lead mb-4">As Black History Month approaches and Black People remain underrepresented in the NFT space, we embarked on a mission to revolutionize the world’s view of NFT’s.</p>
                         <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                            <button type="button" className="btn btn-outline-secondary btn-lg mt-3 px-4">Mint</button>
+                            <Link to="/mint" className="btn btn-outline-secondary btn-lg mt-3 px-4" style={{fontSize: "2rem"}}>Mint</Link>
                         </div>
                     </div>
                 </div>
