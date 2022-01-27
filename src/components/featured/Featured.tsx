088ecd6553ed454e927bb8ae94c6ec846df77edb
Edwin Bryan <bbryan211@gmail.com>
2022-01-27 10:21:10 -0600

    Update number of 500 NFTs

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -20,14 +20,9 @@ function Featured() {
                                     <img src={logo} className="bd-placeholder-img card-img-top img-fluid" width="100%" height="225" alt="Bunks NFT" />
                                 </div>
                             </div>
-                            {/* <div className="col">
-                                <div className="card shadow-sm">
-                                    <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">NFT Thumbnail</text></svg>
-                                </div>
-                            </div> */}
                         </div>
                         <div className="row row-cols-8 mt-4">
-                            <p className="lead text-center">The first 1000 NFT’s for our project will be free, and 6000 will cost .04 in ETH.</p>
+                            <p className="lead text-center">The first 500 NFT’s for our project will be free, and 6500 will cost .04 in ETH.</p>
                         </div>
                     </div>
                 </div>
