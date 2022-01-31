ed1b243f152ff07244131d6f0e472ba6fd901ca3
Edwin Bryan <bbryan211@gmail.com>
2022-01-31 11:40:26 -0600

    Remove unused code

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -51,16 +51,6 @@ function Header() {
                     </div>
                 </div>
             </header>
-
-                {/* <div className="container">
-                    <div className="row">
-                        <div className="py-2 my-2">
-                            <button type="button" className="btn btn-outline-secondary btn-lg mt-3 px-4 btn-header">
-                                CONNECT WALLET
-                            </button>
-                        </div>
-                    </div>
-                </div> */}
         </>
     );
 }
