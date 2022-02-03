a0444145b40c89cfe2e371fae3a96e91237088c2
Edwin Bryan <bbryan211@gmail.com>
2022-02-03 17:17:03 -0600

    Add whitepaper page

---
+++ src/components/whitepaper/Whitepaper.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Header from '../common/header/Header';
+
+function Whitepaper() {
+    return(
+        <>
+            <Header />
+
+            <div className="section">
+                <div className="py-3">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-8">
+                                <h2 className="text-center mb-4" style={{fontStyle: "italic", fontWeight: "bold"}}>Whitepaper</h2>
+
+                                <ul>
+                                    <li><p className="lead">An essential point of the year is almost upon us…</p></li>
+                                    <li><p className="lead">As Black History Month approaches and Black People remain underrepresented in the NFT space, we embarked on a mission to revolutionize the world’s view of NFT’s.</p></li>
+                                    <li><p className="lead">We want to encourage equal representation in the NFT space, as well as remember and respect the legacies of inspiring African-American and Black figures throughout history.</p></li>
+                                    <li><p className="lead">We need a strong platform to convey this message, so we take inspiration from the most popularized NFT of them all: the Crypto Punks. </p></li>
+                                    <li><p className="lead">To help honor African Americans and Blacks Worldwide, a Crypto Punk derivative called <strong>"Bunks"</strong>, was born.</p></li>
+
+ 
+                                    <li><p className="lead">We proudly present to you the first inspirational, <strong>educational</strong> NFT that shows heartfelt appreciation and love to African-Americans and Black People around the World.</p></li>
+                                    <li><p className="lead">100 1 of 1 Legendaries of Famous Black People throughout history will join our set of “Bunks,” to remind us of the legacies we cherish.</p></li>
+                                    <li><p className="lead">A narrative voice will walk you through the accomplishments of each of these famous figures.</p></li>
+                                    <li><p className="lead">There is a special 1 of 1 that is available ONLY if you have a Legendary…if you crack the code of the scavenger hunt first, this will be the prize…</p></li>
+
+
+                                    <li><p className="lead">The first 500 NFT’s for our project will be free, and 6500 will cost .035 in ETH.</p></li>
+                                    <li><p className="lead">So the project never loses its vision, 5% of royalties will go straight to the cause, and only 2.5% will go to the entire team (7.5% total).</p></li>
+                                    <li><p className="lead">When we sell out, we will donate <strong>20%</strong> sales to Black Communities and DAO. </p></li>
+                                    <li><p className="lead"><strong>Truly impactful amounts</strong> will go to underfunded schools, charities, and programs suggested by the community, as well as the team.</p></li>
+
+                                </ul>
+
+                                <h2 className="text-center mb-4" style={{fontStyle: "italic", fontWeight: "bold"}}>Q2 Plans</h2>
+                                <p className="lead">BunksNFT  will launch their own staking platform, earning $SOUL Tokens (ERC20) that will be listed on a decentralized exchange. A percentage of the minting funds will be used to provide liquidity to this token! </p>
+                                <p className="lead">You can use $SOUL to purchase drops, rare NFTs in the community store, or simply HOLD for the $SOUL!</p>
+                                <p className="lead">We will purchase Netvrk land for 3-D Bunks, you can use $SOUL to Purchase 3d ASSESTS AS WELL !</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Whitepaper;
