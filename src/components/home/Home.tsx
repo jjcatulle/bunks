c75d7f024b5c443a5fd64fc50921469677584f33
jjcatulle <38570952+jjcatulle@users.noreply.github.com>
2022-02-04 22:40:26 -0500

    Update Home.tsx

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -17,7 +17,7 @@ function Home() {
                     <img className="d-block mx-auto mb-4" src={logo} alt="Bunks NFT" width="432" height="312" />
                     <h1 className="display-5 fw-bold visually-hidden">Bunks NFT</h1>
                     <div className="col-lg-6 mx-auto">
-                        <p className="lead mb-4">As Black History Month approaches and Black People remain underrepresented in the NFT space, we embarked on a mission to revolutionize the world’s view of NFT’s. (The next 1000 are Free!)</p>
+                        <p className="lead mb-4">As we participate in Black History Month, Black People remain underrepresented in the NFT space, we embarked on a mission to revolutionize the world’s view of NFT’s. (The next 1000 are Free!)</p>
                         <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
                             <Link to="/mint" className="btn btn-outline-secondary btn-lg mt-3 px-4" style={{fontSize: "2rem"}}>Mint</Link>
                         </div>
